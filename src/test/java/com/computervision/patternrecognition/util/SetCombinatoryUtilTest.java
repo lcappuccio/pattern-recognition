4687dfad77b86a9d83657919f0d5798234062475
Leo <leo@systemexception.org>
2019-03-25 14:21:14 +0100

    extract N size sets from Point list

--- src/test/java/com/computervision/patternrecognition/util/SetCombinatoryUtilTest.java
+++ src/test/java/com/computervision/patternrecognition/util/SetCombinatoryUtilTest.java
@@ -2,9 +2,14 @@ package com.computervision.patternrecognition.util;
 
 import com.computervision.patternrecognition.model.Point;
 import org.junit.Before;
+import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 /**
  * 25/03/2019 12:24
@@ -12,7 +17,7 @@ import java.util.List;
 public class SetCombinatoryUtilTest {
 
 	private List<Point> points;
-	private List<List<Point>> pointsSet_2, pointsSet_3;
+	private List<List<Point>> pointsSet_2 = new ArrayList<>(), pointsSet_3 = new ArrayList<>();
 
 	@Before
 	public void setUp() {
@@ -36,5 +41,33 @@ public class SetCombinatoryUtilTest {
 		pointsSet_3.add(Arrays.asList(pointA, pointB, pointD));
 		pointsSet_3.add(Arrays.asList(pointA, pointC, pointD));
 		pointsSet_3.add(Arrays.asList(pointB, pointC, pointD));
+	}
+
+	@Test
+	public void should_generate_all_combinations_size_2() {
+
+		final List<List<Point>> subsetsOfSizeN = SetCombinatoryUtil.getSubsetsOfGivenSize(2, points);
+
+		assertEquals(6, subsetsOfSizeN.size());
+		assertEquals(pointsSet_2, subsetsOfSizeN);
+	}
+
+	@Test
+	public void should_generate_all_combinations_size_3() {
+
+		final List<List<Point>> subsetsOfSizeN = SetCombinatoryUtil.getSubsetsOfGivenSize(3, points);
+
+		assertEquals(4, subsetsOfSizeN.size());
+		assertEquals(subsetsOfSizeN, pointsSet_3);
+	}
+
+	@Test
+	public void should_generate_all_combinations_from_size_2_to_3() {
+
+		final List<List<Point>> subsetsOfSizeN = SetCombinatoryUtil.getSubsetsOfGivenSizeOrMore(2, points);
+
+		assertEquals(10, subsetsOfSizeN.size());
+		assertTrue(subsetsOfSizeN.containsAll(pointsSet_2));
+		assertTrue(subsetsOfSizeN.containsAll(pointsSet_3));
 	}
 }
