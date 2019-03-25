62b2eaaa6b6a262f2762d748e6b41f3969fc880f
Leo <leo@systemexception.org>
2019-03-25 11:34:56 +0100

    extract method to create Point matrices

--- src/main/java/com/computervision/patternrecognition/util/CollinearPointsUtil.java
+++ src/main/java/com/computervision/patternrecognition/util/CollinearPointsUtil.java
@@ -14,7 +14,8 @@ public class CollinearPointsUtil {
 	 * A set of three or more distinct points are collinear if and only if, the matrix of the coordinates of these
 	 * vectors is of rank 1 or less. (source: https://en.wikipedia.org/wiki/Collinearity)
 	 *
-	 * @return
+	 * @param pointList a {@link List<Point>} to determine collinearity
+	 * @return true if collinear
 	 */
 	public static boolean isPointSetCollinear(final List<Point> pointList) {
 
@@ -26,15 +27,27 @@ public class CollinearPointsUtil {
 			return true;
 		}
 
+		double[][] pointMatrix = pointListToMatrix(pointList);
+		final Matrix matrix = new Matrix(pointMatrix);
+		int rank = matrix.rank();
+
+		return rank <= 1;
+	}
+
+	/**
+	 * Convert a {@link List<Point>} to a bidimensional Point matrix
+	 *
+	 * @param pointList
+	 * @return
+	 */
+	private static double[][] pointListToMatrix(final List<Point> pointList) {
+
 		double[][] pointsMatrix = new double[pointList.size()][2];
 		for (int i = 0; i < pointList.size(); i++) {
 			final Point point = pointList.get(i);
 			pointsMatrix[i][0] = point.getX();
 			pointsMatrix[i][1] = point.getY();
 		}
-		final Matrix matrix = new Matrix(pointsMatrix);
-		int rank = matrix.rank();
-
-		return rank <= 1;
+		return pointsMatrix;
 	}
 }
