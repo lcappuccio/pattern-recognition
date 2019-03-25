374b29742b85f72efced47eb4c48b1a1946f2521
Leo <leo@systemexception.org>
2019-03-25 10:00:56 +0100

    add point domain object

---
+++ src/main/java/com/computervision/patternrecognition/model/Point.java
@@ -0,0 +1,19 @@
+package com.computervision.patternrecognition.model;
+
+public class Point {
+
+	private final double x,y;
+
+	public Point(final double x, final double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+}
