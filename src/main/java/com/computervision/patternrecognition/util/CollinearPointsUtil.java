c26e2ae426a6234690368e2fba99d4b083bff844
Leo <leo@systemexception.org>
2019-03-25 11:28:20 +0100

    add collinearity utility class

---
+++ src/main/java/com/computervision/patternrecognition/util/CollinearPointsUtil.java
@@ -0,0 +1,40 @@
+package com.computervision.patternrecognition.util;
+
+import Jama.Matrix;
+import com.computervision.patternrecognition.model.Point;
+
+import java.util.List;
+
+/**
+ * 25/03/2019 11:06
+ */
+public class CollinearPointsUtil {
+
+	/**
+	 * A set of three or more distinct points are collinear if and only if, the matrix of the coordinates of these
+	 * vectors is of rank 1 or less. (source: https://en.wikipedia.org/wiki/Collinearity)
+	 *
+	 * @return
+	 */
+	public static boolean isPointSetCollinear(final List<Point> pointList) {
+
+		if (pointList == null) {
+			throw new IllegalArgumentException();
+		}
+
+		if (pointList.size() == 2) {
+			return true;
+		}
+
+		double[][] pointsMatrix = new double[pointList.size()][2];
+		for (int i = 0; i < pointList.size(); i++) {
+			final Point point = pointList.get(i);
+			pointsMatrix[i][0] = point.getX();
+			pointsMatrix[i][1] = point.getY();
+		}
+		final Matrix matrix = new Matrix(pointsMatrix);
+		int rank = matrix.rank();
+
+		return rank <= 1;
+	}
+}
---
+++ src/test/java/com/computervision/patternrecognition/util/CollinearPointsUtilTest.java
@@ -0,0 +1,94 @@
+package com.computervision.patternrecognition.util;
+
+import com.computervision.patternrecognition.model.Point;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * 25/03/2019 11:11
+ */
+public class CollinearPointsUtilTest {
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+
+	@Test
+	public void invalid_argument() {
+
+		expectedException.expect(IllegalArgumentException.class);
+		//noinspection ConstantConditions
+		CollinearPointsUtil.isPointSetCollinear(null);
+	}
+
+	@Test
+	public void two_points_are_always_collinear() {
+		final Point pointA = new Point(0, 0);
+		final Point pointB = new Point(0.1, 0.2);
+		final List<Point> pointList = Arrays.asList(pointA, pointB);
+
+		boolean isPointSetCollinear = CollinearPointsUtil.isPointSetCollinear(pointList);
+
+		assertTrue("Two points are always collinear", isPointSetCollinear);
+	}
+
+	@Test
+	public void should_be_collinear() {
+
+		final Point pointA = new Point(0, 0);
+		final Point pointB = new Point(1, 1);
+		final Point pointC = new Point(2, 2);
+		final List<Point> pointList = Arrays.asList(pointA, pointB, pointC);
+
+		boolean isPointSetCollinear = CollinearPointsUtil.isPointSetCollinear(pointList);
+
+		assertTrue("Point set is collinear but returns false", isPointSetCollinear);
+	}
+
+	@Test
+	public void should_be_collinear_with_negative() {
+
+		final Point pointA = new Point(0, 0);
+		final Point pointB = new Point(1, 1);
+		final Point pointC = new Point(2, 2);
+		final Point pointNegatives = new Point(-2, -2);
+		final List<Point> pointList = Arrays.asList(pointA, pointB, pointC, pointNegatives);
+
+		boolean isPointSetCollinear = CollinearPointsUtil.isPointSetCollinear(pointList);
+
+		assertTrue("Point set is collinear but returns false", isPointSetCollinear);
+	}
+
+	@Test
+	public void should_be_collinear_only_negatives() {
+
+		final Point pointA = new Point(0, 0);
+		final Point pointB = new Point(-1, -1);
+		final Point pointC = new Point(-2, -2);
+		final Point pointNegatives = new Point(-8, -8);
+		final List<Point> pointList = Arrays.asList(pointA, pointB, pointC, pointNegatives);
+
+		boolean isPointSetCollinear = CollinearPointsUtil.isPointSetCollinear(pointList);
+
+		assertTrue("Point set is collinear but returns false", isPointSetCollinear);
+	}
+
+	@Test
+	public void should_not_be_collinear() {
+
+		final Point pointA = new Point(0, 0);
+		final Point pointB = new Point(1, 1);
+		final Point pointC = new Point(1, 3);
+		final List<Point> pointList = Arrays.asList(pointA, pointB, pointC);
+
+		boolean isPointSetCollinear = CollinearPointsUtil.isPointSetCollinear(pointList);
+
+		assertFalse("Point set is not collinear but returns true", isPointSetCollinear);
+	}
+}
